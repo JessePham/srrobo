4bbecfafa76976a8aa18cd6a285c95461e6f57b1
Christina Nguyen <rawrstinaan@gmail.com>
2020-03-04 17:34:48 -0800

    webcam + motors update
    -app connection to webcam ip+port works
    -need to debug the python code
    -need to start coding the buttons to move the motors

--- SRRobo/app/src/main/java/com/example/srrobo/GetLiveStream.java
+++ SRRobo/app/src/main/java/com/example/srrobo/GetLiveStream.java
@@ -1,12 +1,24 @@
 package com.example.srrobo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.MediaController;
+import android.webkit.*;
 import android.widget.Toast;
 import android.widget.VideoView;
 import android.net.Uri;
@@ -14,8 +26,10 @@ public class GetLiveStream extends AppCompatActivity {
 
     EditText addrField;
     Button btnConnect;
-    VideoView streamView;
-    MediaController mediaController;
+    WebView streamView;
+    ImageButton upBtn;
+    public static String CMD = "0";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +37,9 @@ public class GetLiveStream extends AppCompatActivity {
         setContentView(R.layout.activity_get_live_stream);
 
         addrField = (EditText)findViewById(R.id.addr);
-        btnConnect = (Button)findViewById(R.id.connect);
-        streamView = (VideoView)findViewById(R.id.streamview);
+        streamView = (WebView)findViewById(R.id.webview);
 
+        btnConnect = (Button)findViewById(R.id.connect);
         btnConnect.setOnClickListener(new OnClickListener(){
 
             @Override
@@ -33,6 +47,17 @@ public class GetLiveStream extends AppCompatActivity {
                 String s = addrField.getEditableText().toString();
                 playStream(s);
             }});
+
+        upBtn = (ImageButton)findViewById(R.id.goFowardBtn);
+        upBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CMD = "Forward";
+                System.out.println(GetGPSLocation.wifiModuleIp);
+                Socket_AsyncTask cmd_go_foward = new Socket_AsyncTask();
+                cmd_go_foward.execute();
+            }
+        });
 
     }
 
@@ -43,11 +68,12 @@ public class GetLiveStream extends AppCompatActivity {
                     "UriSrc == null", Toast.LENGTH_LONG).show();
         }else{
 
-            Uri aTest = Uri.parse("http://10.251.111.161:8081/");
-            streamView.setVideoURI(aTest);
-            mediaController = new MediaController(this);
-            streamView.setMediaController(mediaController);
-            streamView.start();
+
+            //Uri aTest = Uri.parse("http://10.251.111.161:8081/");
+            //Uri aTest = Uri.parse("http://www.ted.com/talks/download/video/8584/talk/761");
+            String aTest = "http://10.251.111.161:8081/";
+            streamView.loadUrl(aTest);
+            //add code to auto add "http://" - required to run
 
             Toast.makeText(GetLiveStream.this,
                     "Connect: " + src,
@@ -58,7 +84,26 @@ public class GetLiveStream extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        streamView.stopPlayback();
+    }
+
+    public class Socket_AsyncTask extends AsyncTask<Void,Void,Void>
+    {
+        Socket socket;
+
+        @Override
+        protected Void doInBackground(Void... params){
+            try{
+                InetAddress inetAddress = InetAddress.getByName("10.251.111.161");
+                //10.251.111.161
+                socket = new java.net.Socket(inetAddress,21567);
+                //21567
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeBytes(CMD);
+                dataOutputStream.close();
+                socket.close();
+            }catch (UnknownHostException e){e.printStackTrace();}catch (IOException e){e.printStackTrace();}
+            return null;
+        }
     }
 
 }
