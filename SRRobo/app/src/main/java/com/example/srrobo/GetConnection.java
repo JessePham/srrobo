30b35635845d438995842935c3e8c385c81a5150
Christina Nguyen <rawrstinaan@gmail.com>
2020-04-30 18:39:15 -0700

    Finalized Mobile Application Vers 1.0
    nice one

--- SRRobo/app/src/main/java/com/example/srrobo/GetConnection.java
+++ SRRobo/app/src/main/java/com/example/srrobo/GetConnection.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,23 +12,10 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class GetConnection extends AppCompatActivity {
     //UI Element
@@ -48,7 +34,7 @@ public class GetConnection extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     TextView test, status;
 
-    private static final String PORT_NO = "21567";
+    private static final String PORT_NUM = "21567";
     private DatabaseReference databaseRef;
 
 
@@ -66,6 +52,8 @@ public class GetConnection extends AppCompatActivity {
 
 
         databaseRef = FirebaseDatabase.getInstance().getReference("ipAddress");
+        test.setText("IP Address: " + sharedPreferences.getString(IP, null));
+
 
 
         connect.setOnClickListener(new View.OnClickListener() {
@@ -74,12 +62,7 @@ public class GetConnection extends AppCompatActivity {
                 ipText = txtAddress.getText().toString();
                 if (ipText.matches("[0-9.]*")) {
                     getIPandPort();
-                    //submitPost();
                     databaseRef.setValue(ipText);
-
-                    //CMD = "GetGPS";
-                    //Socket_AsyncTask cmd_increase_servo = new Socket_AsyncTask();
-                    // cmd_increase_servo.execute();
 
                 } else {
 
@@ -95,30 +78,22 @@ public class GetConnection extends AppCompatActivity {
 
     public void getIPandPort() {
 
-        String iPandPort = ipText + ":" + PORT_NO;
-        Log.d("MYTEST", "IP String: " + iPandPort);
+        String iPandPort = ipText + ":" + PORT_NUM;
+        Log.d("TEST", "IP String: " + iPandPort);
         String temp[] = iPandPort.split(":");
         wifiModuleIp = temp[0];
         wifiModulePort = Integer.valueOf(temp[1]);
-        Log.d("MY TEST", "IP:" + wifiModuleIp);
-        Log.d("MY TEST", "PORT:" + wifiModulePort);
-
+        Log.d("test", "IP:" + wifiModuleIp);
+        Log.d("test", "PORT:" + wifiModulePort);
 
         //adds the IP and Port number into the shared preferences folder
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(IP, ipText);
         editor.commit();
 
-        test.setText(sharedPreferences.getString("IP Address:" + IP, null));
-        //status.setText(sharedPreferences.getString("STATUS:" + IP, null));
+        System.out.println(sharedPreferences.getString("IP Address:" + ipText, null));
 
-
-        // Write a message to the database
-
-
-        String reference = "Hello World";
-        Object obj = (Object) reference;
-        // databaseRef.setValueAsync(obj);
+        test.setText("IP Address: " + sharedPreferences.getString(IP, null));
 
 
 
